9cfb1626ecac41a90a0828b3cfda7242c38d3cb7
Leonid <leo-turko@mail.ru>
2023-04-13 11:56:46 +0300

    Add task solving

---
+++ Task/Program.cs
@@ -0,0 +1,26 @@
+﻿int size = 0;
+
+while(size <= 0) {
+    Console.Write("How many lines do you want to input? ");
+    size = Convert.ToInt32(Console.ReadLine());
+}
+
+string[] array = new string[size];
+string[] newArray = {};
+int counter = 0;
+
+for (int i = 0; i < array.Length; i += 1) {
+    Console.Write($"Input {i + 1} line: ");
+    array[i] = Convert.ToString(Console.ReadLine());
+
+    if (array[i].Length <= 3) {
+        Array.Resize(ref newArray, counter + 1);
+        newArray[counter] = array[i];
+        counter += 1;
+    }
+}
+
+Console.WriteLine("Starting Array: ");
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine("New array: ");
+Console.WriteLine($"[{string.Join(", ", newArray)}]");
